937a5943266e424bb19e00ffb49cd74941378e2d
Markus Pedersen <markus@markus2.com>
2020-03-18 12:17:15 +0100

    Added type hints

--- copy_emails.py
+++ copy_emails.py
@@ -1,6 +1,6 @@
 import pyperclip
 
-def copy_emails_from_group(n, filename="emails_real", sepr="\n"):
+def copy_emails_from_group(n : int, filename: str="emails_real", sepr: str="\n") -> None:
 	pyperclip.copy(f"{sepr}".join([email.split("\t")[0] for email in open(filename, "r").read().split("\n") if email.split("\t")[-1] == str(n)]))
 
 
