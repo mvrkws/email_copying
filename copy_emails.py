bcdffe8d101ad07b7fe5d13632c2c8e7e8895957
Markus Pedersen <markus@markus2.com>
2020-03-18 12:21:33 +0100

    Facilitated importing the function

--- copy_emails.py
+++ copy_emails.py
@@ -1,7 +1,7 @@
-def copy_emails_from_group(n: int, filename: str="emails_real", sepr: str="\n") -> None:
+def copy_emails_for_group(n: int, filename: str="emails_real", sepr: str="\n") -> None:
 	import pyperclip
 	pyperclip.copy(f"{sepr}".join([email.split("\t")[0] for email in open(filename, "r").read().split("\n") if email.split("\t")[-1] == str(n)]))
 
 
 if __name__ == "__main__":
-	copy_emails_from_group(2)
+	copy_emails_for_group(2)
