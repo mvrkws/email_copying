a1397430308d9fb8a6a3f34eb3790c41b12ad900
Markus Pedersen <markus@markus2.com>
2020-03-18 11:58:55 +0100

    Converted to list comprehension

--- copy_emails.py
+++ copy_emails.py
@@ -1,17 +1,7 @@
 import pyperclip
 
 def copy_emails_from_group(n, sepr="\n"):
-	copy = ""
+	pyperclip.copy(f"{sepr}".join([email.split("\t")[0] for email in open("emails_real", "r").read().split("\n") if email.split("\t")[-1] == str(n)]))
 
-	for email in open("emails_real", "r").read().split("\n"):
-		try:
-			email, group = email.split("\t")
-		except:
-			continue
-
-		if group == str(n):
-			copy += email + sepr
-
-	pyperclip.copy(copy[:-len(sepr)])
 
 copy_emails_from_group(2)
