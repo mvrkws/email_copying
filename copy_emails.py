25613e7a84185eab6ae3122e8e7139678090dc0a
Markus Pedersen <markus@markus2.com>
2020-03-18 12:10:51 +0100

    Easier to change filename if emails_real is not the preferred name for file

--- copy_emails.py
+++ copy_emails.py
@@ -1,7 +1,7 @@
 import pyperclip
 
-def copy_emails_from_group(n, sepr="\n"):
-	pyperclip.copy(f"{sepr}".join([email.split("\t")[0] for email in open("emails_real", "r").read().split("\n") if email.split("\t")[-1] == str(n)]))
+def copy_emails_from_group(n, filename="emails_real", sepr="\n"):
+	pyperclip.copy(f"{sepr}".join([email.split("\t")[0] for email in open(filename, "r").read().split("\n") if email.split("\t")[-1] == str(n)]))
 
 
 copy_emails_from_group(2)
